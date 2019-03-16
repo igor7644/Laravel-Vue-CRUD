6605a10f1ab1755959718599899182cfc87a37b7
Igor Milosavljević <igor.milosavljevic.72.14@ict.edu.rs>
2019-03-16 20:57:16 +0100

    Imported Vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,13 @@
 require('./bootstrap');
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Vuetify from 'vuetify';
 import router from './routes';
 
 window.Vue = require('vue');
 
 Vue.use(VueRouter);
+Vue.use(Vuetify);
 
 const app = new Vue({
     el: '#app',
