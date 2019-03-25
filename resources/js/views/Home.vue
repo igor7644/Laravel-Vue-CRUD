4758c3bc22294778774de8fe0b26863b79b460b8
Igor Milosavljević <igor.milosavljevic.72.14@ict.edu.rs>
2019-03-25 13:18:34 +0100

    Added navbar and drawer

---
+++ resources/js/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-app>
+
+        <v-toolbar>
+            <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title>CRUD</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn flat><router-link to="/home">Home</router-link></v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+
+        <v-navigation-drawer v-model="drawer">
+            <p>test</p>
+        </v-navigation-drawer>
+        
+    </v-app>
+</template>
+
+<script>
+    export default {
+        
+        data(){
+            return{
+                drawer: false
+            }
+        }
+
+    }
+</script>
