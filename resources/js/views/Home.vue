355698c08de12bed96f87a45e4b7850f5000271b
Igor Milosavljević <igor.milosavljevic.72.14@ict.edu.rs>
2019-03-26 00:06:08 +0100

    Imported toolbar

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,30 +1,27 @@
 <template>
     <v-app>
 
-        <v-toolbar>
-            <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
-            <v-toolbar-title>CRUD</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-toolbar-items class="hidden-sm-and-down">
-            <v-btn flat><router-link to="/home">Home</router-link></v-btn>
-            </v-toolbar-items>
-        </v-toolbar>
-
-        <v-navigation-drawer v-model="drawer">
-            <p>test</p>
-        </v-navigation-drawer>
+        <toolbar></toolbar>
         
     </v-app>
 </template>
 
 <script>
+
+    import toolbar from './components/Toolbar';
+
     export default {
         
         data(){
             return{
-                drawer: false
+                
             }
+        },
+
+        components: {
+            toolbar
         }
 
     }
+    
 </script>
