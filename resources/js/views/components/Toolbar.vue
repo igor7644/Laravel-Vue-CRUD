64bfd19cde67a6aab0d9eb5c2a627a8f017aae45
Igor Milosavljević <igor.milosavljevic.72.14@ict.edu.rs>
2019-03-26 21:17:31 +0100

    Fixed v-list tag

--- resources/js/views/components/Toolbar.vue
+++ resources/js/views/components/Toolbar.vue
@@ -16,9 +16,9 @@
                     <v-list-tile-action>
                         <v-icon class="black--text"> {{ link.icon }} </v-icon>
                     </v-list-tile-action>
-                    <v-list-title-content>
+                    <v-list-tile-content>
                         <v-list-tile-title class="black--text"> {{ link.text }} </v-list-tile-title>
-                    </v-list-title-content>
+                    </v-list-tile-content>
                 </v-list-tile>
             </v-list>
         </v-navigation-drawer>
