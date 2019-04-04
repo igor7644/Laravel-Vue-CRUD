6247b7145de2967766dfa36b590b7a45028694ad
Igor Milosavljević <igor.milosavljevic.72.14@ict.edu.rs>
2019-04-04 16:47:08 +0200

    Removed tooltip on add user

--- resources/js/views/Users.vue
+++ resources/js/views/Users.vue
@@ -7,18 +7,13 @@
                 <v-toolbar flat color="white">
                 <v-toolbar-title>USERS</v-toolbar-title>
                 <v-spacer></v-spacer>
-                <v-dialog dark v-model="dialog" max-width="900px">
+                <v-dialog persistent dark v-model="dialog" max-width="900px">
                     <template v-slot:activator="{ on }">
-                        <v-tooltip top>
-                            <template v-slot:activator="{ on }">
-                                <v-btn color="primary" dark fab small class="mt-4" v-on="on">
-                                    <i class="material-icons">
-                                        add
-                                    </i>
-                                </v-btn>
-                            </template>
-                        <span>Add User</span>
-                        </v-tooltip>
+                        <v-btn color="primary" dark fab small class="mt-4" v-on="on">
+                            <i class="material-icons">
+                                add
+                            </i>
+                        </v-btn>
                     </template>
                     <v-card>
                     <v-card-text>
@@ -64,11 +59,11 @@
                     <td class="justify-center px-0">
                         <v-tooltip right>
                             <template v-slot:activator="{ on }">
-                            <v-btn color="primary" small round dark @click="editUser(props.item)" v-on="on">
-                                <v-icon size="20">
-                                    edit    
-                                </v-icon>
-                            </v-btn>
+                                <v-btn color="primary" small round dark @click="editUser(props.item)" v-on="on">
+                                    <v-icon size="20">
+                                        edit    
+                                    </v-icon>
+                                </v-btn>
                             </template>
                             <span>Edit User</span>
                         </v-tooltip>
