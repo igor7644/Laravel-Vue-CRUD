94838d68a74e9bbe43aeba4aeb48462d59cfa0ff
Igor Milosavljević <igor.milosavljevic.72.14@ict.edu.rs>
2019-04-05 22:57:55 +0200

    Finished Dashboard

--- resources/js/views/Dashboard.vue
+++ resources/js/views/Dashboard.vue
@@ -2,7 +2,58 @@
     <v-app>
 
         <v-container class="my-5">
-            <h1 class="blue--text">DASHBOARD</h1> 
+
+            <v-layout pa-2>
+                <h1>Welcome to CRUD SPA!</h1>
+            </v-layout>
+            <v-layout pa-2>
+                <h2>Choose bellow:</h2>
+            </v-layout>
+            
+            <v-layout row>
+                <v-flex class="text-center ma-5">
+                    <v-card class="primary" hover>
+                        <v-layout justify-center>
+                        <v-card-title primary-title>
+                        <div>
+                            <div class="headline">Users</div>
+                            <span>Explore all users</span>
+                        </div>
+                        </v-card-title>
+                        </v-layout>
+                        <v-card-actions>
+                            <v-btn color="white" to="/users"> 
+                                Explore 
+                                <i class="material-icons">
+                                keyboard_arrow_right
+                                </i>
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+
+                <v-flex class="text-center ma-5">
+                    <v-card class="primary" hover>
+                        <v-layout justify-center>
+                        <v-card-title primary-title>
+                        <div>
+                            <div class="headline">Posts</div>
+                            <span>Explore all posts</span>
+                        </div>
+                        </v-card-title>
+                        </v-layout>
+                        <v-card-actions>
+                            <v-btn color="white" to="/posts"> 
+                                Explore 
+                                <i class="material-icons">
+                                keyboard_arrow_right
+                                </i>
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+            
         </v-container>   
 
     </v-app>
