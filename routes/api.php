247df85b4515b6b1188cc2642b9659c75da97389
Igor Milosavljević <igor.milosavljevic.72.14@ict.edu.rs>
2019-04-05 20:23:48 +0200

    User create route

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ Route::middleware('api')->group(function () {
     Route::get('/users', 'UserController@index');
     Route::post('/user/{user}/destroy', 'UserController@destroy');
     Route::post('/user/{user}/edit', 'UserController@edit');
+    Route::post('/user/create', 'UserController@create');
 
 });
 
