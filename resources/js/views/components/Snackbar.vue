7af47655f370351c0adea338c3b1c64dd01b1977
Igor Milosavljević <igor.milosavljevic.72.14@ict.edu.rs>
2019-04-05 13:50:09 +0200

    Show snackbar when user-deleted

---
+++ resources/js/views/components/Snackbar.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-app>
+        <v-snackbar
+            v-model="snackbar"
+            color="primary"
+            :bottom="y === 'bottom'"
+            :left="x === 'left'"
+            :multi-line="mode === 'multi-line'"
+            :right="x === 'right'"
+            :timeout="timeout"
+            :top="y === 'top'"
+            :vertical="mode === 'vertical'"
+        >
+      {{ text }}
+      <v-btn
+            color="black"
+            flat
+            @click="snackbar = false"
+      >
+        Close
+      </v-btn>
+    </v-snackbar>
+    </v-app>
+</template>
+
+<script>
+  
+    export default {
+
+        data () {
+            return {
+                snackbar: false,
+                y: 'bottom',
+                x: null,
+                mode: '',
+                timeout: 4000,
+                text: ''
+            }
+        },
+
+        mounted(){
+            let self = this;
+
+            Event.$on('user-deleted', function(message){
+                self.text = message;
+                self.snackbar = true;
+            });
+        }
+
+    }
+
+
+</script>
+
+
